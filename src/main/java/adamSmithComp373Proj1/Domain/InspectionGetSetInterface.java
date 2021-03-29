49c46aca981ea5d0849beab8e63c4112f001203c
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-29 15:00:25 -0500

    created InspectionGetSetInterface.java

---
+++ src/main/java/adamSmithComp373Proj1/Domain/InspectionGetSetInterface.java
@@ -0,0 +1,37 @@
+package adamSmithComp373Proj1.Domain;
+
+public interface InspectionGetSetInterface {
+    
+
+
+    //Get functionalities
+
+
+
+
+public String getName(){
+}
+public Type getType(){
+}
+public String getDetails(){
+}
+public Integer getFacility(){
+}
+public Integer getRoom(){
+}
+
+//Set functionalities
+
+public void setName(){
+}
+public void setType(){      
+}
+public void setDetails(){    
+}
+public void setFacility(){    
+}
+public void setRoom(){  
+}
+
+
+}
