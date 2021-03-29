486b040e3ceca12185a44d5adb5d0c58fb12092d
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-29 14:06:07 -0500

    update inspection

--- src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
+++ src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
@@ -1,66 +1,33 @@
 package adamSmithComp373Proj1.Domain;
 import adamSmithComp373Proj1.DAL.InspectionDAO;
 import adamSmithComp373Proj1.Domain.Facility;
-
+import java.util.ArrayList;
 public enum Type{TYPE, TYPE2, TYPE3}
 
-public class Inspection {
+public class Inspection extends InspectionGetSetInterface{
     
-    private InspectionDAO Conn = new InspectionDAO();
-    private Integer InspectionID;
-    private String Name = Conn.getName();
-    private Type InsType = Conn.getType();
-    private String Details = Conn.getDetails();
-    private Integer facility = Conn.getFacility();
-    private Integer Room = Conn.getRoom();
     
-
-
-    
-    public void addInspection(){
-        
+    public void addInspection(){   
     }
-
-
-
     public String getName(){
-        return this.Name;
     }
-
     public Type getType(){
-        return this.InsType;
     }
-
     public String getDetails(){
-        return this.Details;
     }
-
     public Integer getFacility(){
-        return this.facility;
     }
-
     public Integer getRoom(){
-        return this.Room;
     }
-
     public void setName(){
-
     }
-
-    public void setType(){
-        
+    public void setType(){      
     }
-
-    public void setDetails(){
-        
+    public void setDetails(){    
     }
-
-    public void setFacility(){
-        
+    public void setFacility(){    
     }
-
-    public void setRoom(){
-        
+    public void setRoom(){  
     }
 
 
