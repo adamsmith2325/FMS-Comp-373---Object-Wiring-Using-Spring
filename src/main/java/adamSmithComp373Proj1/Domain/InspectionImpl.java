119fbc7437a58284ab132e2a8016fafbb230ed0b
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-29 15:20:21 -0500

    updated basic attributes

--- src/main/java/adamSmithComp373Proj1/Domain/InspectionImpl.java
+++ src/main/java/adamSmithComp373Proj1/Domain/InspectionImpl.java
@@ -5,4 +5,20 @@ import adamSmithComp373Proj1.Domain.Facility;
 public enum Type{TYPE, TYPE2, TYPE3}
 
 public class Inspection {
+    package adamSmithComp373Proj1.Domain;
+import adamSmithComp373Proj1.DAL.InspectionDAO;
+import adamSmithComp373Proj1.Domain.Facility;
+
+public enum Type{TYPE, TYPE2, TYPE3}
+
+public class Inspection {
+    
+    private InspectionDAO Conn = new InspectionDAO();
+    private Integer InspectionID;
+    private String Name = Conn.getName();
+    private Type InsType = Conn.getType();
+    private String Details = Conn.getDetails();
+    private Integer facility = Conn.getFacility();
+    private Integer Room = Conn.getRoom();
+    
 }
