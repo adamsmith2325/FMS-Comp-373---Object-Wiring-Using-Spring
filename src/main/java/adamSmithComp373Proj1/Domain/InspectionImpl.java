3f0cb8df2e2af750c34cb9f5324c3e278b0cee7d
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-29 15:20:41 -0500

    last update duplicated the code for some reason

--- src/main/java/adamSmithComp373Proj1/Domain/InspectionImpl.java
+++ src/main/java/adamSmithComp373Proj1/Domain/InspectionImpl.java
@@ -4,14 +4,6 @@ import adamSmithComp373Proj1.Domain.Facility;
 
 public enum Type{TYPE, TYPE2, TYPE3}
 
-public class Inspection {
-    package adamSmithComp373Proj1.Domain;
-import adamSmithComp373Proj1.DAL.InspectionDAO;
-import adamSmithComp373Proj1.Domain.Facility;
-
-public enum Type{TYPE, TYPE2, TYPE3}
-
-public class Inspection {
     
     private InspectionDAO Conn = new InspectionDAO();
     private Integer InspectionID;
